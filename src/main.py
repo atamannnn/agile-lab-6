31b493e50ff45a35bbe6a6afa7cd0cff465ba135
atamannnn <168690428+atamannnn@users.noreply.github.com>
2025-11-30 11:45:06 +0200

    Add comment for Jira integration testing
    Added a comment for testing Jira integration.

--- src/main.py
+++ src/main.py
@@ -5,3 +5,5 @@ if __name__ == "__main__":
     x = int(input("Enter first number: "))
     y = int(input("Enter second number: "))
     print(f"Sum is: {add(x, y)}")
+
+# Test commit for Jira integration
